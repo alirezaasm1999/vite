93544c406ca75250c6ab7e48d5321b08483c3994
alireza-asmaee <alireza.asmaee.dev@gmail.com>
2024-08-20 20:47:14 +0330

    sdfsdddddddddddddd

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(async () => {
       // mkcert(),
     ],
     build: {
-      outDir: "./build",
+      outDir: "./dist",
       emptyOutDir: true,
       rollupOptions: {
         output: {
